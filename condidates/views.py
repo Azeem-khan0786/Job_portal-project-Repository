92d388dcfd6cde38ec81780d7fcefe6080900ecd
azeemkhan <ak9828129@gmail.com>
2024-10-03 11:53:02 +0530

    hafjafgagf

--- condidates/views.py
+++ condidates/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,HttpResponseRedirect ,redirect
 from condidates.forms import CondidateRegisteration
 from django.http import HttpResponse
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.forms import authenticationForm
+from django.contrib import messsages
 
 
 # Create your views here.
@@ -17,3 +20,8 @@ def registration(request):
         form = CondidateRegisteration()
 
     return render(request, 'Condidates/registration.html', {'form': form})
+
+# login page for condidates
+def Login(request):
+    if request.method=='POST':
+        form =AuthenticationForm(request,request.POST)
