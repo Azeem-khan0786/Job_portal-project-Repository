3056bc5136453654191334960089ee7548650e7d
azeemkhan <ak9828129@gmail.com>
2024-10-08 18:26:26 +0530

    update files

--- job_portal/urls.py
+++ job_portal/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/',include('condidates.urls')),
-    
+     # Redirect the root URL to the admin page
+    path('', RedirectView.as_view(url='/admin/', permanent=True)),
    
 ]
 if settings.DEBUG:
